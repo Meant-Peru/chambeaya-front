f5d8b9b5861736b16c89f5232ab6d0d45bf70bf2
cnahuina <cnahuina@gmail.com>
2022-04-25 00:09:53 -0500

    upload base scss

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './sass/main.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+        Prueba chambeaya
+        </p>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
