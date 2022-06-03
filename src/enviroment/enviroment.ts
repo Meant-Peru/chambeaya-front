d964a12b911a91df9630703669dc9c9e05dfd473
cnahuina <cnahuina@gmail.com>
2022-06-03 16:16:26 -0500

    enviroment

--- src/enviroment/enviroment.ts
+++ src/enviroment/enviroment.ts
@@ -1,2 +1,3 @@
 // export const URI = "http://159.65.41.204:4009"
-export const URI = 'http://localhost:3500';
+// export const URI = 'http://localhost:3500';
+export const URI = 'http://api.chambea-ya.com';
