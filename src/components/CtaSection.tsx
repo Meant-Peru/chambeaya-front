9619f437379cbf7824ce110efdff6023f8d040aa
stefanny <stefannysalasjimenez@gmail.com>
2022-07-18 16:09:18 -0500

    CH-17-CAMBIOS-REGISTRARME

--- src/components/CtaSection.tsx
+++ src/components/CtaSection.tsx
@@ -1,15 +1,25 @@
 import React from "react";
 import "./../sass/components/_ctaSection.scss";
 import { BtnPrimary } from "./shared/styled";
+import {useNavigate} from "react-router-dom";
 
 export default function CtaSection() {
+
+    const navigate = useNavigate();
+    /**
+     * Handle redirects to user register form.
+     */
+    const handleRedirect = () => {
+        navigate("/register")
+    }
+
     return (
         <React.Fragment>
             <div className="sectionFull">
                 <section className="CtaSection">
                     <h2>Descubre opciones seguras de 
                         encontrar <br /> proyectos pensados para tí</h2>
-                    <BtnPrimary> REGISTRARME </BtnPrimary>
+                    <BtnPrimary onClick={handleRedirect}> REGISTRARME </BtnPrimary>
                 </section>
             </div>
         </React.Fragment>
