3d80f74d4e7ccaa73491df07fdaa13b7e7fa29db
aliciaperalta <peraltalinaris27@gmail.com>
2022-09-09 15:50:47 -0500

    validation login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 import Header from '../components/shared/header';
 import { useNavigate } from 'react-router-dom';
-import { Txtfield, BtnPrimary, BtnSecondary } from './../components/shared/styled';
+import {Txtfield, BtnPrimary, BtnSecondary, Span} from './../components/shared/styled';
 import Modal from 'react-modal';
 
 import './../sass/pages/_login.scss';
@@ -39,13 +39,43 @@ export default function Login() {
 	}
 
 	const navigate = useNavigate();
-	const [user, setUser] = React.useState({ email: '', password: '' });
+	const [user, setUser] = React.useState({
+		email: '',
+		password: '',
+		formErrors: []});
 
 	const { startLogin } = useAuth();
 
-	const handleRedirect = async () => await startLogin(user);
+	const handleRedirect = async (e: any) => {
+		if (Object.entries(user.formErrors).map(([key, val]) => val).every(element => element === false) && Object.entries(user.formErrors).length == 2) {
+			await startLogin(user);
+		}
+	}
 
 	const handleEvent = (e: any) => {
+		let validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+		if (e.target.name === 'email') {
+			if (!e.target.value.match(validRegex)) {
+				e.target.classList.add('has-error-email');
+				e.target.nextSibling.nextSibling.classList.add('has-error-description-email');
+			} else {
+				e.target.classList.remove('has-error-email');
+				e.target.nextSibling.nextSibling.classList.remove('has-error-description-email');
+			}
+		}
+
+		if (e.target.value == '' && !e.target.classList.contains('has-error')) {
+			user.formErrors[e.target.name] = true;
+			e.target.classList.add('has-error');
+			e.target.nextSibling.classList.add('has-error-description');
+			if (e.target.classList.contains('has-error-email')) {
+				e.target.nextSibling.nextSibling.classList.remove('has-error-description-email');
+			}
+		} else if (e.target.value !== '' && e.target.hasOwnProperty('value')) {
+			user.formErrors[e.target.name] = false;
+			e.target.classList.remove('has-error');
+			e.target.nextSibling.classList.remove('has-error-description');
+		}
 		setUser({
 			...user,
 			[e.target.name]: e.target.value,
@@ -59,7 +89,10 @@ export default function Login() {
 				<h2>Ingresar a la plataforma</h2>
 				<aside className="FormGroup mt-5">
 					<Txtfield onChange={handleEvent} name="email" className="mb-3" placeholder="Usuario" />
+					<Span className="error-required-field-description"> * Por favor ingresa tu usuario. </Span>
+					<Span className="error-required-field-description">* Por favor ingresa el formato correcto. </Span>
 					<Txtfield onChange={handleEvent} name="password" className="mb-2" type={'password'} placeholder="Contraseña" />
+					<Span className="error-required-field-description"> * Por favor ingresa tu contraseña. </Span>
 				</aside>
 				<aside className="FormGroup">
 					<p>
@@ -78,7 +111,7 @@ export default function Login() {
 				
 				</Modal>
 				<aside className="FormAction mt-5">
-					<BtnPrimary onClick={handleRedirect}>Login</BtnPrimary>
+					<BtnPrimary onClick={handleRedirect} className={`button-primary ${Object.entries(user.formErrors).map( ([key, val]) => val).every(element => element === false) && Object.entries(user.formErrors).length == 2 ? 'test' : 'disabled'}`}>Login</BtnPrimary>
 					<p className="mt-2">
 						<a href="/register">¿No tienes una cuenta?</a>
 					</p>
